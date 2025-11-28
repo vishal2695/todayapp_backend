01b537d169acfbb571a5d8961d6d53bd4db71bdf
vishaldubey <vkd2695@gmail.com>
2025-11-28 12:55:40 +0530

    chnges

--- today/settings.py
+++ today/settings.py
@@ -103,23 +103,23 @@ WSGI_APPLICATION = 'today.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "today_db",
-#         "USER": "root",
-#         "PASSWORD": "",
-#         "HOST": "localhost",
-#         "PORT": "3306",   
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "todayapp_db",
+        "USER": "root",
+        "PASSWORD": "Root123!@#",
+        "HOST": "localhost",
+        "PORT": "3306",   
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
